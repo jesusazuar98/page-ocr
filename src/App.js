04b9d48fe844b9799006025cd0b7d835c36d4fa9
Jesus Adrian Sanchez Azuar <jesusazuar@gmail.com>
2023-05-10 19:41:27 +0200

    App-ocr finish

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { createWorker } from "tesseract.js";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [selectImage, setImage] = useState(null);
+  const [textConverter, setTextConverter] = useState("");
+
+  const convertText = async () => {
+    if (selectImage !== null) {
+      const worker = await createWorker();
+
+      await worker.loadLanguage("spa");
+      await worker.initialize("spa");
+
+      const { data } = await worker.recognize(selectImage);
+      await worker.terminate();
+      setTextConverter(data.text);
+    }
+  };
+
+  useEffect(() => {
+    convertText();
+  }, [selectImage]);
+
+  const handleImage = (e) => {
+    setImage(e.target.files[0]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="content">
+        <h1>Covertidor OCR</h1>
+        <label>Selecciona un archivo para convertirlo en texto</label>
+        <br />
+        <input type="file" accept="image/*" onChange={handleImage} />
+
+        <div className={selectImage !== null ? "info-content" : ""}>
+          {selectImage && (
+            <div className="image-container">
+              <img src={URL.createObjectURL(selectImage)} alt="img" />
+            </div>
+          )}
+          {textConverter && (
+            <div className="text-container">
+              <p>{textConverter}</p>
+            </div>
+          )}
+        </div>
+      </div>
+    </>
   );
 }
 
